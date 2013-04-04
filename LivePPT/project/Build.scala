9b3546c72d2b25c9bcd214c44bb0a5b379f3a023
Bowen Liang <simonlbw@gmail.com>
2013-04-04 16:09:21 +0800

    加入mysql驱动和application.conf中的数据库配置信息。

--- LivePPT/project/Build.scala
+++ LivePPT/project/Build.scala
@@ -11,7 +11,8 @@ object ApplicationBuild extends Build {
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    "mysql" % "mysql-connector-java" % "5.1.18"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
