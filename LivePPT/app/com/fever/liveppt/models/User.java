548d4315ebf6c95b3c1007079291a291b02be5b1
Bowen Liang <simonlbw@gmail.com>
2013-04-06 09:03:03 +0800

    完成signup页面的注册请求发出和返回结果处理。成功后自动跳转到myppt页面。

--- LivePPT/app/com/fever/liveppt/models/User.java
+++ LivePPT/app/com/fever/liveppt/models/User.java
@@ -1,32 +1,52 @@
 package com.fever.liveppt.models;
 
+import java.util.List;
+
 import javax.persistence.*;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.Query;
 
 import play.db.ebean.*;
 import play.data.validation.*;
 
+/**
+ * 用户类
+ * 
+ * @author Bowen Liang
+ * 
+ */
 @Entity
 public class User extends Model {
 
 	@Id
 	public Long id;
-	
+
 	@Constraints.Required
 	public String email;
-	
+
 	@Constraints.Required
 	public String password;
-	
+
 	public String displayname;
-	
-	public static Finder<Long,User> find = new Finder<Long,User>(
-		    Long.class, User.class
-		  ); 
-	
-	public User(String email, String password, String displayname){
+
+	public static Finder<Long, User> find = new Finder<Long, User>(Long.class,
+			User.class);
+
+	public User(String email, String password, String displayname) {
 		this.email = email;
 		this.password = password;
 		this.displayname = displayname;
+	}
+
+	public static boolean isExistedByEmail(String email) {
+//		Query<User> q = Ebean.createQuery(User.class);
+		List<User> existedUsers = User.find.where().eq("email", email).findList();
+		if (existedUsers.size() > 0) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 }
--- LivePPT/public/js/signup.js
+++ LivePPT/public/js/signup.js
@@ -4,28 +4,35 @@ define(function(require, exports, module) {
 
 	var alertBox = $("#alertBox");
 
+	alertBox.hide();
+
 	$('.btn-signup-submit').on('click', function(e){
 		e.preventDefault();
 
+		//通过Ajax发出注册请求
 		$.ajax({
 			url: '/signup',
-			type: 'post',			
+			type: 'post',
+			dataType: 'json',
 			data: {
 				email: $('#inputEmail').val(),
 				password: $('#inputPassword').val(),
 				displayname: $('#inputDisplayName').val()
 			},
-			success: function(res){
-				console.log(res);
-				$("#alertBox span").html(res);
-				alertBox.alert();
-
+			success: function(res, status){
+				if (res.isSuccess){
+					//注册成功，跳转页面
+					window.location.href = '/myppt';
+				} else {
+					//注册不成功，显示错误信息
+					$("#alertBox span").html(res.message);
+					alertBox.show();
+				}
 			},
 			error: function(res){
 				console.log(res);
 			}
 		});
-		console.log("signup");
 	});
 
 });
