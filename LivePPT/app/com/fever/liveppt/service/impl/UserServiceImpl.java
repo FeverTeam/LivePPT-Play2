9514df3fcc568b21da5e16fca618bfff8bc3fc3a
Bowen Liang <simonlbw@gmail.com>
2013-05-23 01:23:54 +0800

    1、更正用户存在却返回“用户不存在”逻辑错误；2、重写验证用户是否存在的逻辑，减少对数据库的读操作。

--- LivePPT/app/com/fever/liveppt/service/impl/UserServiceImpl.java
+++ LivePPT/app/com/fever/liveppt/service/impl/UserServiceImpl.java
@@ -9,23 +9,23 @@ import com.fever.liveppt.service.UserService;
 import com.fever.liveppt.utils.JsonResult;
 import com.fever.liveppt.utils.StatusCode;
 
-
 public class UserServiceImpl implements UserService {
 
 	public ObjectNode isPassworrdCorrect(String email, String password) {
 		ObjectNode jsonNode;
-		
-		//验证用户是否存在
-		if (User.isExistedByEmail(email)==true){
+
+		// 验证用户是否存在
+		User user = User.find.where().eq("email", email).findUnique();
+		if (user == null) {
+			// 用户不存在
 			// 封装返回信息,用户不存在
-			jsonNode = new JsonResult(false,StatusCode.USER_NOT_EXISTED).setMessage("用户不存在");
-		}else 
-		{
+			jsonNode = new JsonResult(false, StatusCode.USER_NOT_EXISTED, "用户不存在");
+		} else {
+			// 用户存在
 			// 验证用户密码
-			User user = User.isPasswordCorrect(email, password);
-			if (user != null) {
+			if (user.password.equals(password)) {
 				ObjectNode data = Json.newObject();
-				// 验证成功
+				// 密码验证成功
 				data.put("userId", user.id);
 				data.put("displayName", user.displayname);
 
@@ -34,8 +34,8 @@ public class UserServiceImpl implements UserService {
 			} else {
 
 				// 封装返回信息
-				jsonNode = new JsonResult(false, StatusCode.USER_PASSWORD_ERROR).setMessage("密码错误");
-			}	
+				jsonNode = new JsonResult(false, StatusCode.USER_PASSWORD_ERROR, "密码错误");
+			}
 		}
 		return jsonNode;
 	}
@@ -43,19 +43,18 @@ public class UserServiceImpl implements UserService {
 	@Override
 	public JsonResult register(String email, String password, String displayName) {
 		// TODO Auto-generated method stub
-		
-		//判断是否已存在相同用户
-		if (User.isExistedByEmail(email)==true){
-			//注册失败，已存在相同email帐号的用户
-			return new JsonResult(false, StatusCode.USER_EXISTED).setMessage("已存在相同Email的帐号。");
+
+		// 判断是否已存在相同用户
+		if (User.isExistedByEmail(email) == true) {
+			// 注册失败，已存在相同email帐号的用户
+			return new JsonResult(false, StatusCode.USER_EXISTED)
+					.setMessage("已存在相同Email的帐号。");
 		} else {
-			//注册新用户
+			// 注册新用户
 			User user = new User(email, password, displayName);
 			user.save();
 			return new JsonResult(true).setMessage("注册成功");
 		}
 	}
-
-
 
 }
