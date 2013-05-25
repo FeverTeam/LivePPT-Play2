c9bbabec9e47b31cc77db5a42a5dbcb5344afb5c
Bowen Liang <simonlbw@gmail.com>
2013-05-25 21:04:42 +0800

    改进Akka任务分配中Duration的规范写法。

--- LivePPT/app/controllers/MeetingController.java
+++ LivePPT/app/controllers/MeetingController.java
@@ -112,7 +112,7 @@ public class MeetingController extends Controller {
 						final String cacheKey=Long.toString(meetingId);   
    
 						Akka.system().scheduler().schedule(
-							Duration.create(0, TimeUnit.MILLISECONDS),
+							Duration.Zero(),
 							Duration.create(10, TimeUnit.MILLISECONDS), 
 							new Runnable(){
 								Long currentIndex;
