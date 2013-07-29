72f1bfe9ba757afd9449eb5f7e537ea069cb74d9
WeiJie Li <licatweijie@gmail.com>
2013-07-30 00:32:46 +0800

    添加controller.iron.UserController#regist(), 提供注册的contro

--- LivePPT-2/app/com/liveppt/configs/InjectionConfigModule.java
+++ LivePPT-2/app/com/liveppt/configs/InjectionConfigModule.java
@@ -1,6 +1,8 @@
 package com.liveppt.configs;
 
 import com.google.inject.AbstractModule;
+import com.liveppt.services.UserService;
+import com.liveppt.services.impl.UserServiceImpl;
 
 /**
  * 配置依赖绑定关系
@@ -15,7 +17,7 @@ public class InjectionConfigModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		// TODO Auto-generated method stub
-		// bind().to();
+		bind(UserService.class).to(UserServiceImpl.class);
 	}
 	
 }
