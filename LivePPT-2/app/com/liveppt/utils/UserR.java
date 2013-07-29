c5363512f7a4c5290e04dd2629c9e411e6a32f42
WeiJie Li <licatweijie@gmail.com>
2013-07-30 00:00:34 +0800

    添加com.liveppt.utils.UserR类，用于作为User的下级可读写载体

---
+++ LivePPT-2/app/com/liveppt/utils/UserR.java
@@ -0,0 +1,19 @@
+package com.liveppt.utils;
+
+/**
+ * 用户类载体
+ * author 黎伟杰
+ */
+public class UserR {
+
+    public Long id;
+
+    public String email;
+
+    public String password;
+
+    public String displayname;
+
+    public int status;
+
+}
