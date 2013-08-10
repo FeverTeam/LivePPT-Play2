9e3d293164a6b2fe2270c7e85272a9727baf955a
JOHNBUG <zzyjohnbug@gmail.com>
2013-08-11 02:51:12 +0800

    用户注册页面Demo

--- LivePPT-2/public/javascripts/app/config.js
+++ LivePPT-2/public/javascripts/app/config.js
@@ -1,9 +1,10 @@
 seajs.config({
 	alias: {
-		'jquery': '/assets/javascripts/jquery/jquery-1.9.0.min.js',
-		'underscore': '/assets/javascripts/backbone/underscore.js',
-		'backbone': '/assets/javascripts/backbone/backbone.js' ,
+		'jquery'     : '/assets/javascripts/jquery/jquery-1.9.0.min.js',
+		'underscore' : '/assets/javascripts/backbone/underscore.js',
+		'backbone'   : '/assets/javascripts/backbone/backbone.js' ,
         'bootstrap2' : '/assets/javascripts/bootstrap/2/bootstrap.min.js',
-        'bootstrap3' : '/assets/javascripts/bootstrap/3/bootstrap.min.js'
+        'bootstrap3' : '/assets/javascripts/bootstrap/3/bootstrap.min.js',
+        'signup'     : '/assets/javascripts/app/signup.js'
 	}
 });
--- LivePPT-2/public/javascripts/app/main.js
+++ LivePPT-2/public/javascripts/app/main.js
@@ -3,24 +3,4 @@ define(function(require, exports, module){
 	require('underscore');
     require('backbone');
     require('bootstrap3');
-    /*
-	var indexPage = Backbone.View.extend({
-    	el: $("body"),
-    	events: {
-    		"click #test": "toggleSidebar"
-    	},
- 		toggleSidebar: function(){
- 			var left = $("#xs-innerFrame").css("left");
-			if(left == "0px"){
-				$("#xs-innerFrame").animate({left: "-22%"}, 1000).animate({left: "-20.9%"}, 300);
-                $("#test").text("Hide Me");
-            }
-			else {
-                $("#xs-innerFrame").animate({left: "0"}, 1000);
-                $("#test").text("Menu");
-            }
- 		}
-	});  
-    var app = new indexPage;
-    */
 });
