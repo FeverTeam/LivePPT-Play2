0aa72b40dfde6917d2714a1bf30fb220a1c951d4
WeiJie Li <licatweijie@gmail.com>
2013-08-22 10:53:00 +0800

    重写UserReader

--- LivePPT-2/app/com/liveppt/utils/models/UserReader.java
+++ LivePPT-2/app/com/liveppt/utils/models/UserReader.java
@@ -15,7 +15,7 @@ public class UserReader {
 
     /**
      * 产生UserReader类
-     * @param params
+     * @param
      * @return
      * last modified 黎伟杰
      */
@@ -28,19 +28,17 @@ public class UserReader {
         newDisplay = null;
     }
 
-    public Long id;
+    private Long id;
 
-    public String email;
+    private String email;
 
-    public String password;
+    private String password;
 
-    public String newPassword;
+    private String newPassword;
 
-    public String display;
+    private String display;
 
-    public String newDisplay;
-
-    public Map<String, String[]> params;
+    private String newDisplay;
 
     /**
      * 设置Id
@@ -65,66 +63,106 @@ public class UserReader {
 
     /**
      * 设置email
-     * @return
-     * @throws EmailNotFoundException
+     * @return 本身
      * last modified 黎伟杰
      */
-    public UserReader setEmail() throws EmailNotFoundException {
-        String email = params.get(UserJson.KEY_EMAIL)[0];
-        if (email==null) throw  new EmailNotFoundException();
+    public UserReader setEmail(String email)  {
         this.email = email;
         return this;
     }
 
     /**
-     * 通过params设置password
-     * @return
-     * @throws PasswordNotFoundException
+     * 得到email
+     * @return email
+     * @throws EmailNotFoundException
      * last modified 黎伟杰
      */
-    public UserReader setPassword() throws PasswordNotFoundException {
-        String password = params.get(UserJson.KEY_PASSWORD)[0];
-        if (password==null) throw  new PasswordNotFoundException();
+    public String getEmail() throws EmailNotFoundException {
+        if (email==null) throw  new EmailNotFoundException();
+        return email;
+    }
+
+    /**
+     * 设置password
+     * @return itself
+     * last modified 黎伟杰
+     */
+    public UserReader setPassword(String password)  {
         this.password = password;
         return this;
     }
 
     /**
-     * 通过params设置newPassword
-     * @return
-     * @throws NewPasswordNotFoundException
+     * 获得password
+     * @return password
+     * @throws PasswordNotFoundException
      * last modified 黎伟杰
      */
-    public UserReader setNewPassword() throws NewPasswordNotFoundException {
-        String newPassword = params.get(UserJson.KEY_NEW_PASSWORD)[0];
-        if (newPassword==null) throw  new NewPasswordNotFoundException();
+    public String getPassword() throws PasswordNotFoundException {
+        if (this.password==null) throw new PasswordNotFoundException();
+        return password;
+    }
+
+    /**
+     * 设置newPassword
+     * @return
+     * last modified 黎伟杰
+     */
+    public UserReader setNewPassword(String newPassword) {
         this.newPassword = newPassword;
         return this;
     }
 
     /**
-     * 通过params设置display
+     * 获得newPassword
      * @return
-     * @throws DisplayNotFoundException
+     * @throws NewPasswordNotFoundException
      * last modified 黎伟杰
      */
-    public UserReader setDisplay() throws DisplayNotFoundException {
-        String display = params.get(UserJson.KEY_DISPLAY)[0];
-        if (display==null) throw  new DisplayNotFoundException();
+    public String getNewPassword() throws NewPasswordNotFoundException {
+        if (this.newPassword==null) throw new NewPasswordNotFoundException();
+        return this.newPassword ;
+
+    }
+
+    /**
+     * 设置display
+     * @return
+     * last modified 黎伟杰
+     */
+    public UserReader setDisplay(String display)  {
         this.display = display;
         return this;
     }
 
     /**
-     * 通过params设置newPisplay
+     * 获得display
+     * @return
+     * @throws DisplayNotFoundException
+     * last modified 黎伟杰
+     */
+    public String getDisplay() throws DisplayNotFoundException  {
+        if (this.display==null) throw new DisplayNotFoundException();
+        return this.display ;
+    }
+
+    /**
+     * 设置newPisplay
+     * last modified 黎伟杰
+     */
+    public UserReader setNewDisplay(String newDisplay) {
+        this.newDisplay = newDisplay;
+        return this;
+    }
+
+    /**
+     * 获得newPisplay
      * @return
      * @throws NewDisplayNotFoundException
      * last modified 黎伟杰
      */
-    public UserReader setNewDisplay() throws NewDisplayNotFoundException {
-        String newDisplay = params.get(UserJson.KEY_NEW_PASSWORD)[0];
+    public String getNewDisplay() throws NewDisplayNotFoundException {
         if (newDisplay==null) throw  new NewDisplayNotFoundException();
-        this.newDisplay = newDisplay;
-        return this;
+        return  this.newDisplay;
     }
 }
