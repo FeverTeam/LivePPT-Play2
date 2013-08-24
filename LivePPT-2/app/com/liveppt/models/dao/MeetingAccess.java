ffdbbe651985881810e83649fb733031a540e379
lizijing <itzijing@gmail.com>
2013-08-24 23:43:07 +0800

    MeetingAcess中添加获取meeting信息接口和setPage接口

--- LivePPT-2/app/com/liveppt/models/dao/MeetingAccess.java
+++ LivePPT-2/app/com/liveppt/models/dao/MeetingAccess.java
@@ -9,9 +9,11 @@ import java.util.List;
 
 import com.liveppt.models.Attender;
 import com.liveppt.models.Meeting;
+import com.liveppt.models.Ppt;
 import com.liveppt.models.User;
 import com.liveppt.utils.exception.meeting.MeetingIdErrorException;
 import com.liveppt.utils.exception.meeting.MeetingPermissionDenyException;
+import com.liveppt.utils.exception.ppt.PptPermissionDenyException;
 import com.liveppt.utils.models.MeetingReader;
 
 
@@ -132,5 +134,79 @@ public class MeetingAccess {
         }
 		return new MeetingReader(meeting);
         
+    }
+    /**
+     * 得到会议信息
+     * @param meetingReader
+     * @return MeetingReader
+     * last modified Zijing Lee
+     * @throws MeetingPermissionDenyException 
+     */
+    public static MeetingReader getMeetingInfoByFounder(MeetingReader meetingReader) throws MeetingPermissionDenyException{
+    	Meeting meeting = Meeting.find.byId(meetingReader.getMeetingId());
+    	if(meetingReader.getUserId().equals(meeting.founder.id))
+    	{
+    		meetingReader.setPptId(meeting.ppt.id);
+    		meetingReader.setTopic(meeting.topic);
+    		meetingReader.setCurrentPageIndex(meeting.currentPageIndex);
+    	}
+    	else
+    	{
+            throw new MeetingPermissionDenyException();
+        }
+        return meetingReader;
+    }
+    /**
+     * 得到会议信息
+     * @param meetingReader
+     * @return MeetingReader,userId
+     * last modified Zijing Lee
+     * @throws MeetingPermissionDenyException 
+     */
+    public static MeetingReader getMeetingInfoByAttender(MeetingReader meetingReader,Long userId) throws MeetingPermissionDenyException
+    {   
+    	Meeting meeting = Meeting.find.byId(meetingReader.getMeetingId());
+    	List<Attender> attenderList = meeting.attenders;
+    	boolean flag = false;
+    	for(Attender attending : attenderList)
+    	{
+    		if(attending.id.equals(userId))
+    		{
+    			flag = true;
+    			break;
+    		}
+    	}
+    	//若此人非Meeting参与者，拒绝提供信息
+    	if(flag)
+    	{
+    		meetingReader.setPptId(meeting.ppt.id);
+    		meetingReader.setTopic(meeting.topic);
+    		meetingReader.setCurrentPageIndex(meeting.currentPageIndex);
+    	}
+    	else
+    	{
+    		throw new MeetingPermissionDenyException();
+    	}
+    	return meetingReader;
+    }
+    /**
+     * 获取会议信息
+     * @param meetingReader
+     * @return MeetingReader
+     * last modified Zijing Lee
+     */
+    public static MeetingReader setMeetingPage(MeetingReader meetingReader) throws MeetingPermissionDenyException{
+    	Meeting meeting = Meeting.find.byId(meetingReader.getMeetingId());
+    	//若此人非Meeting操作者，拒绝操作
+    	if(meetingReader.getUserId().equals(meeting.founder.id))
+    	{
+    	meeting.currentPageIndex = meetingReader.getCurrentPageIndex();
+    	meeting.save();
+    	}
+    	else
+    	{
+    		throw new MeetingPermissionDenyException();
+    	}
+    	return meetingReader;
     }
 }
