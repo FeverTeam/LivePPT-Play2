0f4fd0296a9d0d0eef5086bfa71a5d24358ff04d
Bowen Liang <simonlbw@gmail.com>
2013-10-15 23:22:23 +0800

    开启应用级gzip功能

--- LivePPT/app/Global.java
+++ LivePPT/app/Global.java
@@ -3,14 +3,22 @@ import play.GlobalSettings;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import play.api.mvc.EssentialFilter;
+import play.filters.gzip.GzipFilter;
 
 public class Global extends GlobalSettings {
 
 	private static final Injector INJECTOR = createInjector();
 
+    @Override
+    public <T extends EssentialFilter> Class<T>[] filters() {
+        //开启Gzip压缩
+        return new Class[]{GzipFilter.class};
+    }
+
 	@Override
-	public <A> A getControllerInstance(Class<A> controllerClass)
-			throws Exception {
+	public <A> A getControllerInstance(Class<A> controllerClass) throws Exception {
+        //为controller提供Guice依赖注入
 		return INJECTOR.getInstance(controllerClass);
 	}
 
