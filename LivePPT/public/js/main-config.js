293809407009e61041ff0d5175b2f17b355f1a8c
Bowen Liang <simonlbw@gmail.com>
2013-11-15 23:01:26 +0800

    升级bootstrap为3.0.2

--- LivePPT/public/js/main-config.js
+++ LivePPT/public/js/main-config.js
@@ -3,7 +3,7 @@ seajs.config({
 	alias: {
 		'jquery': {src:'/assets/js/jquery-1.9.1.min.js', exports:'$'},
 		'cookies': '/assets/js/jquery.cookie.js',
-		'bootstrap': 'http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js',
+		'bootstrap': 'http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js',
 		'fineuploader': '/assets/js/fineuploader/jquery.fineuploader-3.4.1.min.js',
 		'uploadify': '/assets/js/jquery.uploadify.min.js',
 		'uploadifive': '/assets/js/jquery.uploadifive_customized.js',
